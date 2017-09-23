034dcece6d2e9d1486bd943b82a4689fdf410dab
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2017-09-23 22:06:06 +1000

    Initial Commit

---
+++ MusicTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicTestProject
+//
+//  Created by Adam Dorogi-Kaposi on 23/9/17.
+//  Copyright © 2017 Adam Dorogi-Kaposi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
